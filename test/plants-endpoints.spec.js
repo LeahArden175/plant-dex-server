bdd5d26e560531cf745a8527ad9b2984f26e5f9c
Leah Arden <leaharden175@gmail.com>
2020-11-10 09:00:36 -0500

    working on tests

--- test/plants-endpoints.spec.js
+++ test/plants-endpoints.spec.js
@@ -1,7 +1,38 @@
 const { expect } = require('chai')
 const knex = require('knex')
 const app = require('../src/app')
+const supertest = require('supertest')
+const {makePlantInfoArray} = require('./plants.fixtures')
 
 describe('Plants Endpoints', function() {
+
+    let db
+
+    before("make knex instance", () => {
+        db = knex({
+          client: "pg",
+          connection: process.env.TEST_DATABASE_URL,
+        });
+        app.set("db", db);
+      });
+
+    after('disconnect from db', () => {
+        db.raw('TRUNCATE plant_dex_users, plant_dex_plant_info RESTART IDENTITY CASCADE')
+    })
+
+    before('clean the table', () => {
+        db.raw('TRUNCATE plant_dex_users, plant_dex_plant_info RESTART IDENTITY CASCADE')
+
+    })
+
+    describe.only('GET /api/plants', () => {
+        context('Given no plants', () => {
+            it('responds with 200 and an empty list', () => {
+                return supertest(app)
+                    .get('/api/plants')
+                    .expect(200, [])
+            })
+        })
+    })
 
 })
---
+++ test/plants.fixtures.js
@@ -0,0 +1,32 @@
+function makePlantInfoArray() {
+    return [
+   {
+     "id": "1",
+     "nickName": 'UFO Plant',
+     "scientificName": 'Pilea Peperomiodes',
+     "datePurchased": '2029-01-22T16:28:32.615Z',
+     "purchasePlace": 'Father Natures',
+     "user_id": '1'
+   },
+   {
+     "id": "2",
+     "nickName": 'Musaica',
+     "scientificName": 'Calathea Musaica',
+     "datePurchased": '2029-01-22T16:28:32.615Z',
+     "purchasePlace": 'Plant Vine',
+     "user_id": '1'
+   },
+   {
+    "id": "3",
+    "nickName": 'Monstera',
+    "scientificName": 'Monstera Deliciosa',
+    "datePurchased": '2029-01-22T16:28:32.615Z',
+    "purchasePlace": 'Plant Vine',
+    "user_id": '1'
+   }
+ ]
+}
+
+module.exports = {
+    makePlantInfoArray,
+}
