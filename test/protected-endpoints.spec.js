d08c314698f665148d2975afc70d331748346cd0
Leah Arden <leaharden175@gmail.com>
2020-11-13 12:04:42 -0500

    protected endpoints tests complete

---
+++ test/protected-endpoints.spec.js
@@ -0,0 +1,61 @@
+const knex = require("knex");
+const app = require("../src/app");
+const helpers = require("./test-helpers");
+const supertest = require("supertest");
+
+describe("Protected endpoints", function () {
+  let db;
+
+  const { testUsers, testPlants } = helpers.makePlantsFixtures();
+
+  before("make knex instance", () => {
+    db = knex({
+      client: "pg",
+      connection: process.env.TEST_DATABASE_URL,
+    });
+    app.set("db", db);
+  });
+
+  after("disconnect from db", () => db.destroy());
+
+  before("cleanup", () => helpers.cleanTables(db));
+
+  afterEach("cleanup", () => helpers.cleanTables(db));
+
+  beforeEach('insert plants', () => helpers.seedPlantsTables(db,testUsers,testPlants))
+
+  const protectedEndpoints = [
+      {
+          name: 'GET /api/plants/:plant_id',
+          path: '/api/plants/1',
+          method: supertest(app).get
+      },
+      {
+        name: 'POST /api/plants',
+        path: '/api/plants',
+        method: supertest(app).post
+    }
+  ]
+
+  protectedEndpoints.forEach(endpoint => {
+      describe.only(endpoint.name, () => {
+          it('responds with 401 missing bearer token when no bearer token', () => {
+              return endpoint.method(endpoint.path)
+                .expect(401, {error: `Missing bearer token`})
+          })
+          it('responds with 401 unauthorized request when invalid JWT secret', () => {
+              const validUser = testUsers[0]
+              const invalidSecret = 'WRONG'
+              return endpoint.method(endpoint.path)
+                .set('Authorization', helpers.makeAuthHeader(validUser, invalidSecret))
+                .expect(401, {error: 'Unauthorized request'})
+            })
+            it('responds with 401 unauthorized request when invaid sub in payload', () => {
+                const invalidUser = {username: 'WRONG', id: 1}
+                return endpoint.method(endpoint.path)
+                    .set('Authorization', helpers.makeAuthHeader(invalidUser))
+                    .expect(401, {error: 'Unauthorized request'})
+            })
+      })
+  })
+});
--- test/test-helpers.js
+++ test/test-helpers.js
@@ -34,23 +34,23 @@ function makeUsersArray() {
     {
       username: "dunder",
       full_name: "Dunder Mifflin",
-      nickname: 'test',
+      nickname: "test",
       password: "password",
-      id: 1
+      id: 1,
     },
     {
       username: "leah",
       full_name: "Leah Arden",
       nickname: "leah",
       password: "password",
-      id: 2
+      id: 2,
     },
     {
       username: "scoops",
       full_name: "Nick Dardaris",
       nickname: "scoops",
       password: "password",
-      id: 3
+      id: 3,
     },
   ];
 }
@@ -84,26 +84,28 @@ function makeMaliciousPlant() {
   };
 }
 function makePlantsFixtures() {
-    const testUsers = makeUsersArray()
-    const testPlants = makePlantInfoArray(testUsers)
-    return {testPlants, testUsers}
+  const testUsers = makeUsersArray();
+  const testPlants = makePlantInfoArray(testUsers);
+  return { testPlants, testUsers };
 }
 function seedUsers(db, users) {
-    const preppedUsers = users.map(user => ({
-        ...user,
-        password: bcrypt.hashSync(user.password, 1)
-    }))
-    return db.into('plant_dex_users').insert(preppedUsers)
-        .then(() =>{
-            db.raw(
-                `SELECT setval('plant_dex_users_id_seq, ?)`,
-                [users[users.length -1].id]
-            )
-        })
+  const preppedUsers = users.map((user) => ({
+    ...user,
+    password: bcrypt.hashSync(user.password, 1),
+  }));
+  return db
+    .into("plant_dex_users")
+    .insert(preppedUsers)
+    .then(() => {
+      db.raw(`SELECT setval('plant_dex_users_id_seq, ?)`, [
+        users[users.length - 1].id,
+      ]);
+    });
 }
 function cleanTables(db) {
-    return db.transaction(trx =>
-      trx.raw(
+  return db.transaction((trx) =>
+    trx
+      .raw(
         `TRUNCATE
           plant_dex_plant_info,
           plant_dex_users
@@ -111,14 +113,29 @@ function cleanTables(db) {
       )
       .then(() =>
         Promise.all([
-          trx.raw(`ALTER SEQUENCE plant_dex_plant_info_id_seq minvalue 0 START WITH 1`),
-          trx.raw(`ALTER SEQUENCE plant_dex_users_id_seq minvalue 0 START WITH 1`),
+          trx.raw(
+            `ALTER SEQUENCE plant_dex_plant_info_id_seq minvalue 0 START WITH 1`
+          ),
+          trx.raw(
+            `ALTER SEQUENCE plant_dex_users_id_seq minvalue 0 START WITH 1`
+          ),
           trx.raw(`SELECT setval('plant_dex_plant_info_id_seq', 0)`),
-          trx.raw(`SELECT setval('plant_dex_users_id_seq', 0)`)
+          trx.raw(`SELECT setval('plant_dex_users_id_seq', 0)`),
         ])
       )
-    )
-  }
+  );
+}
+function seedPlantsTables(db, users, plants) {
+  // use a transaction to group the queries and auto rollback on any failure
+  return db.transaction(async (trx) => {
+    await seedUsers(trx, users);
+    await trx.into('plant_dex_plant_info').insert(plants);
+    // update the auto sequence to match the forced id values
+    await trx.raw(`SELECT setval('plant_dex_plant_info_id_seq', ?)`, [
+      plants[plants.length - 1].id,
+    ]);
+  });
+}
 
 module.exports = {
   makePlantInfoArray,
@@ -128,4 +145,5 @@ module.exports = {
   makePlantsFixtures,
   seedUsers,
   cleanTables,
+  seedPlantsTables,
 };
